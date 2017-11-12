5d57e9a604d79e839d8201d34f67ed0e98e78097
gcanti <giulio.canti@gmail.com>
2017-11-12 12:29:19 +0100

    move creation logic into combinators

--- src/PathReporter.ts
+++ src/PathReporter.ts
@@ -1,16 +1,16 @@
 import { Reporter } from './Reporter'
 import { Context, getFunctionName, ValidationError } from './index'
 
-function stringify(value: any): string {
-  return typeof value === 'function' ? getFunctionName(value) : JSON.stringify(value)
+function stringify(v: any): string {
+  return typeof v === 'function' ? getFunctionName(v) : JSON.stringify(v)
 }
 
 function getContextPath(context: Context): string {
   return context.map(({ key, type }) => `${key}: ${type.name}`).join('/')
 }
 
-function getMessage(value: any, context: Context): string {
-  return `Invalid value ${stringify(value)} supplied to ${getContextPath(context)}`
+function getMessage(v: any, context: Context): string {
+  return `Invalid value ${stringify(v)} supplied to ${getContextPath(context)}`
 }
 
 export function failure(es: Array<ValidationError>): Array<string> {
