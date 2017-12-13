1e3c14fa5b21d0e8055c582d3f345616ff1ef5d1
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2017-12-13 13:45:50 +0100

    exhibit that manual array copying way faster than idiomatic js

--- test/perf.ts
+++ test/perf.ts
@@ -1,9 +1,20 @@
-import * as assert from 'assert'
 import * as Benchmark from 'benchmark'
-import { Context, getDefaultContext, getContextEntry, string, number } from '../src/index'
+import * as assert from 'assert'
+
+import { Context, getContextEntry, getDefaultContext, number, string } from '../src/index'
 
 const suite = new Benchmark.Suite()
 const c: Context = getDefaultContext(string)
+
+const appendNext = (arr: any[], e: any) => {
+  const len = arr.length
+  const res = Array(len + 1)
+  for (let i = 0; i < len; i++) {
+    res[i] = arr[i]
+  }
+  res[len] = e
+  return res
+}
 
 describe('perf', () => {
   ;(it('concat([a]) should be faster than concat(a)', done => {
@@ -13,6 +24,9 @@ describe('perf', () => {
       })
       .add('concat([e])', function() {
         c.concat([getContextEntry('key', number)])
+      })
+      .add('manual appendNext function', function() {
+        appendNext(c, getContextEntry('key', number))
       })
       .on('cycle', function(event: any) {
         console.log(String(event.target))
