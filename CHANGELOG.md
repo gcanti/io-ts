8ec98b389be89fb2eafbd99e7f504da3224901e4
gcanti <giulio.canti@gmail.com>
2018-01-29 11:16:26 +0100

    add support for mutually recursive types, closes #114

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,13 @@
 **Note**: Gaps between patch versions are faulty/broken releases. **Note**: A feature tagged as Experimental is in a
 high state of flux, you're at risk of it changing without notice.
 
+# 0.9.6
+
+* **New Feature**
+  * `recursive` combinator
+    * add support for mutually recursive types, closes #114 (@gcanti)
+    * make it safer: `RT` now must extend `Type<mixed, A>` (@gcanti)
+
 # 0.9.5
 
 * **New Feature**
--- test/recursion.ts
+++ test/recursion.ts
@@ -2,36 +2,29 @@ import * as assert from 'assert'
 import * as t from '../src/index'
 import { assertSuccess, assertFailure, assertStrictEqual, DateFromNumber } from './helpers'
 
+type T = {
+  a: number
+  b: T | undefined | null
+}
+const T = t.recursion<T>('T', self =>
+  t.interface({
+    a: t.number,
+    b: t.union([self, t.undefined, t.null])
+  })
+)
+
 describe('recursion', () => {
   it('should succeed validating a valid value', () => {
-    const T = t.recursion('T', self =>
-      t.interface({
-        a: t.number,
-        b: t.union([self, t.undefined, t.null])
-      })
-    )
     assertSuccess(t.validate({ a: 1, b: null }, T))
     assertSuccess(t.validate({ a: 1, b: { a: 2, b: null } }, T))
   })
 
   it('should return the same reference if validation succeeded', () => {
-    const T = t.recursion('T', self =>
-      t.interface({
-        a: t.number,
-        b: t.union([self, t.undefined, t.null])
-      })
-    )
     const value = { a: 1, b: { a: 2, b: null } }
     assertStrictEqual(t.validate(value, T), value)
   })
 
   it('should fail validating an invalid value', () => {
-    const T = t.recursion('T', self =>
-      t.interface({
-        a: t.number,
-        b: t.union([self, t.undefined, t.null])
-      })
-    )
     assertFailure(t.validate(1, T), ['Invalid value 1 supplied to : T'])
     assertFailure(t.validate({}, T), ['Invalid value undefined supplied to : T/a: number'])
     assertFailure(t.validate({ a: 1, b: {} }, T), [
@@ -53,20 +46,7 @@ describe('recursion', () => {
       })
     )
     assert.deepEqual(T.serialize({ a: new Date(0), b: null }), { a: 0, b: null })
-  })
-
-  it('should return the same reference when serializing', () => {
-    type T = {
-      a: number
-      b: T | null
-    }
-    const T = t.recursion<T>('T', self =>
-      t.interface({
-        a: t.number,
-        b: t.union([self, t.null])
-      })
-    )
-    assert.strictEqual(T.serialize, t.identity)
+    assert.deepEqual(T.serialize({ a: new Date(0), b: { a: new Date(1), b: null } }), { a: 0, b: { a: 1, b: null } })
   })
 
   it('should type guard', () => {
@@ -82,5 +62,42 @@ describe('recursion', () => {
     )
     assert.strictEqual(T.is({ a: new Date(0), b: null }), true)
     assert.strictEqual(T.is({ a: 0 }), false)
+  })
+
+  it('should have a `type` field', () => {
+    type T = {
+      a: number
+      b: T | null
+    }
+    const T = t.recursion<T>('T', self =>
+      t.interface({
+        a: t.number,
+        b: t.union([self, t.null])
+      })
+    )
+    assert.strictEqual(T.type instanceof t.Type, true)
+    assert.strictEqual(T.type.name, '{ a: number, b: (T | null) }')
+    assert.strictEqual((T.type as any).props.a._tag, 'NumberType')
+  })
+
+  it('should support mutually recursive types', () => {
+    type A = {
+      b: A | B | null
+    }
+    type B = {
+      a: A | null
+    }
+    const A: t.RecursiveType<t.Any, A> = t.recursion<A>('A', self =>
+      t.interface({
+        b: t.union([self, B, t.null])
+      })
+    )
+    const B = t.recursion<B>('B', () =>
+      t.interface({
+        a: t.union([A, t.null])
+      })
+    )
+    assert.strictEqual(A.is({ b: { b: null } }), true)
+    assert.strictEqual(A.is({ b: { a: { b: { a: null } } } }), true)
   })
 })
