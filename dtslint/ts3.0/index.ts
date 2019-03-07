76f2d70f1b3611d4b007f7acbbd3353f8c07bccd
gcanti <giulio.canti@gmail.com>
2019-03-07 11:04:34 +0100

    dtslint: upgrade interfaceWithOptionals example

--- dtslint/ts3.0/index.ts
+++ dtslint/ts3.0/index.ts
@@ -523,13 +523,7 @@ export function interfaceWithOptionals<RequiredProps extends t.Props, OptionalPr
   required: RequiredProps,
   optional: OptionalProps,
   name?: string
-): t.IntersectionType<
-  [
-    t.InterfaceType<RequiredProps, t.TypeOfProps<RequiredProps>>,
-    t.PartialType<OptionalProps, t.TypeOfPartialProps<OptionalProps>>
-  ],
-  t.TypeOfProps<RequiredProps> & t.TypeOfPartialProps<OptionalProps>
-> {
+): t.IntersectionC<[t.TypeC<RequiredProps>, t.PartialC<OptionalProps>]> {
   return t.intersection([t.type(required), t.partial(optional)], name)
 }
 
@@ -541,33 +535,6 @@ export function maybe<RT extends t.Any>(
 ): t.UnionType<[RT, t.NullType], t.TypeOf<RT> | null, t.OutputOf<RT> | null, t.InputOf<RT> | null> {
   return t.union<[RT, t.NullType]>([type, t.null], name)
 }
-
-// ----------------
-
-const pluck = <F extends string, U extends t.UnionType<Array<t.InterfaceType<{ [K in F]: t.Mixed }>>>>(
-  union: U,
-  field: F
-): t.Type<t.TypeOf<U>[F]> => {
-  return t.union(union.types.map(type => type.props[field]) as any)
-}
-
-export const Action = t.union([
-  t.type({
-    type: t.literal('Action1'),
-    payload: t.type({
-      foo: t.string
-    })
-  }),
-  t.type({
-    type: t.literal('Action2'),
-    payload: t.type({
-      bar: t.string
-    })
-  })
-])
-
-const ActionType = pluck(Action, 'type')
-type Assert20 = t.TypeOf<typeof ActionType> // $ExpectType "Action1" | "Action2"
 
 // ----------------
 
