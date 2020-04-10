a780ccd7215c57f4d76e1f8a072edbfe641705ee
Forbes Lindesay <forbes@lindesay.co.uk>
2020-04-10 17:30:16 +0100

    fix: test case

--- dtslint/after3.8/ts3.8/index.ts
+++ dtslint/after3.8/ts3.8/index.ts
@@ -5,5 +5,5 @@ type Tuple8TypeTest = t.TypeOf<typeof Tuple8> // $ExpectType [string, string, st
 type Tuple8OutputTest = t.OutputOf<typeof Tuple8> // $ExpectType [string, string, string, string, string, string]
 
 const Tuple9 = t.tuple([t.string, t.string, t.boolean, t.string, t.string, t.string, t.string])
-type Tuple9TypeTest = t.TypeOf<typeof Tuple8> // $ExpectType [string, string, boolean, string, string, string, string]
-type Tuple9OutputTest = t.OutputOf<typeof Tuple8> // $ExpectType [string, string, boolean, string, string, string, string]
+type Tuple9TypeTest = t.TypeOf<typeof Tuple9> // $ExpectType [string, string, boolean, string, string, string, string]
+type Tuple9OutputTest = t.OutputOf<typeof Tuple9> // $ExpectType [string, string, boolean, string, string, string, string]
