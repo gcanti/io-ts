0b8983ce759f94f965f1ee180a97d6136ade3be2
gcanti <giulio.canti@gmail.com>
2020-06-21 14:11:19 +0200

    remove FreeDecoder module

--- perf/DecoderT.ts
+++ perf/DecoderT.ts
@@ -2,22 +2,23 @@ import * as Benchmark from 'benchmark'
 import * as E from 'fp-ts/lib/Either'
 import { pipe } from 'fp-ts/lib/pipeable'
 import * as D from '../src/Decoder'
-import * as DE from '../src/DecoderError'
+import * as DE from '../src/DecodeError'
 import * as DT from '../src/DecoderT'
-import * as FD from '../src/FreeDecoder'
 import * as FS from '../src/FreeSemigroup'
 import * as G from '../src/Guard'
+import * as T from 'fp-ts/lib/Tree'
+import * as NEA from 'fp-ts/lib/NonEmptyArray'
 
 /*
 
-Guard (good) x 35,369,047 ops/sec ±1.50% (86 runs sampled)
-Decoder (good) x 4,653,320 ops/sec ±0.76% (87 runs sampled)
-DecoderT (good) x 2,295,214 ops/sec ±1.10% (88 runs sampled)
-Guard (bad) x 35,062,516 ops/sec ±1.48% (82 runs sampled)
-Decoder (bad) x 1,225,890 ops/sec ±0.74% (89 runs sampled)
-DecoderT (bad) x 2,011,330 ops/sec ±0.60% (86 runs sampled)
-Decoder (draw) x 568,278 ops/sec ±2.85% (84 runs sampled)
-DecoderT (draw) x 376,532 ops/sec ±1.78% (85 runs sampled)
+Guard (good) x 34,238,300 ops/sec ±1.99% (82 runs sampled)
+Decoder (good) x 4,617,403 ops/sec ±0.87% (88 runs sampled)
+DecoderT (good) x 2,334,698 ops/sec ±1.04% (87 runs sampled)
+Guard (bad) x 35,743,005 ops/sec ±1.31% (82 runs sampled)
+Decoder (bad) x 1,239,790 ops/sec ±0.65% (91 runs sampled)
+DecoderT (bad) x 1,980,574 ops/sec ±0.51% (88 runs sampled)
+Decoder (draw) x 589,953 ops/sec ±2.11% (87 runs sampled)
+DecoderT (draw) x 380,337 ops/sec ±0.36% (85 runs sampled)
 
 */
 
@@ -37,6 +38,18 @@ function getDecoderT() {
     name: string,
     age: number
   })
+}
+
+function toForest<E>(s: FS.FreeSemigroup<DE.DecodeError<E>>): NEA.NonEmptyArray<T.Tree<string>> {
+  const toTree: (e: DE.DecodeError<E>) => T.Tree<string> = DE.fold({
+    Leaf: (input, error) => T.make(`cannot decode ${JSON.stringify(input)}, should be ${error}`),
+    Required: (key, errors) => T.make(`required property ${JSON.stringify(key)}`, toForest(errors))
+  })
+  const toForest: (f: FS.FreeSemigroup<DE.DecodeError<E>>) => NEA.NonEmptyArray<T.Tree<string>> = FS.fold(
+    (value) => NEA.of(toTree(value)),
+    (left, right) => NEA.concat(toForest(left), toForest(right))
+  )
+  return toForest(s)
 }
 
 const decoderT = getDecoderT()
@@ -84,7 +97,7 @@ suite
   .add('DecoderT (draw)', function () {
     pipe(
       decoderT.decode(bad),
-      E.mapLeft((e) => D.draw(FD.toForest(e)))
+      E.mapLeft((e) => D.draw(toForest(e)))
     )
   })
   .on('cycle', function (event: any) {
