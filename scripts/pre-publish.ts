94cf63fbc039686ea1cddcca52528a2a74aa6972
Omar Diab <me@omardiab.com>
2020-08-08 15:01:28 +0900

    fp-ts import

--- scripts/pre-publish.ts
+++ scripts/pre-publish.ts
@@ -1,4 +1,4 @@
-import { left } from '../src/TaskEither'
+import { left } from 'fp-ts/TaskEither'
 import { run } from './run'
 
 const main = left(new Error('"npm publish" can not be run from root, run "npm run release" instead'))
