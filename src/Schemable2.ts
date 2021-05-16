756b2020bba19cfc38b638b745a0b2ced940cd9b
gcanti <giulio.canti@gmail.com>
2021-05-16 09:15:48 +0200

    more Schemable2

--- src/Schemable2.ts
+++ src/Schemable2.ts
@@ -1,8 +1,20 @@
 import { HKT, Kind, URIS } from 'fp-ts/lib/HKT'
-import * as I from 'fp-ts/lib/Identity'
 import * as D from './poc'
-import { memoize } from './Schemable'
 import * as G from './Guard'
+import * as E from './Eq'
+import { Eq } from 'fp-ts/lib/Eq'
+
+export function memoize<A, B>(f: (a: A) => B): (a: A) => B {
+  const cache = new Map()
+  return (a) => {
+    if (!cache.has(a)) {
+      const b = f(a)
+      cache.set(a, b)
+      return b
+    }
+    return cache.get(a)
+  }
+}
 
 // -------------------------------------------------------------------------------------
 // use case: Schemable
@@ -16,9 +28,6 @@ export interface Schemable<S> {
   readonly tuple: <A extends ReadonlyArray<D.AnyUD>>(
     ...components: { [K in keyof A]: HKT<S, A[K]> }
   ) => HKT<S, D.TupleD<A>>
-  readonly fromTuple: <A extends ReadonlyArray<D.AnyD>>(
-    ...components: { [K in keyof A]: HKT<S, A[K]> }
-  ) => HKT<S, D.FromTupleD<A>>
 }
 
 export interface Schemable1<S extends URIS> {
@@ -29,9 +38,6 @@ export interface Schemable1<S extends URIS> {
   readonly tuple: <A extends ReadonlyArray<D.AnyUD>>(
     ...components: { [K in keyof A]: Kind<S, A[K]> }
   ) => Kind<S, D.TupleD<A>>
-  readonly fromTuple: <A extends ReadonlyArray<D.AnyD>>(
-    ...components: { [K in keyof A]: Kind<S, A[K]> }
-  ) => Kind<S, D.FromTupleD<A>>
 }
 
 export interface Schema<A> {
@@ -47,44 +53,78 @@ export function compile<S>(S: Schemable<S>): <A>(schema: Schema<A>) => HKT<S, A>
   return (schema) => schema(S)
 }
 
-export const toDecoder: Schemable1<I.URI> = {
-  URI: I.URI,
+declare module 'fp-ts/lib/HKT' {
+  interface URItoKind<A> {
+    readonly 'io-ts/toDecoder': A
+  }
+}
+
+export const toDecoder: Schemable1<'io-ts/toDecoder'> = {
+  URI: 'io-ts/toDecoder',
   string: D.string,
   number: D.number,
   nullable: D.nullable,
-  tuple: D.tuple,
-  fromTuple: D.fromTuple
+  tuple: D.tuple
+}
+
+// helper to please ts@3.5
+type ToGuard<I, A> = A extends I ? G.Guard<I, A> : never
+
+declare module 'fp-ts/lib/HKT' {
+  interface URItoKind<A> {
+    readonly 'io-ts/toGuard': ToGuard<D.InputOf<A>, D.TypeOf<A>>
+  }
+}
+
+export const toGuard: Schemable1<'io-ts/toGuard'> = {
+  URI: 'io-ts/toGuard',
+  string: G.string,
+  number: G.number,
+  nullable: G.nullable as any,
+  tuple: G.tuple as any
 }
 
 declare module 'fp-ts/lib/HKT' {
   interface URItoKind<A> {
-    readonly 'io-ts/ToGuard': G.Guard<D.InputOf<A>, D.TypeOf<A> extends D.InputOf<A> ? D.TypeOf<A> : never>
+    readonly 'io-ts/toEq': Eq<D.TypeOf<A>>
   }
 }
 
-export const toGuard: Schemable1<'io-ts/ToGuard'> = {
-  URI: 'io-ts/ToGuard',
-  string: G.string,
-  number: G.number,
-  nullable: G.nullable as any,
-  tuple: G.tuple as any,
-  fromTuple: G.fromTuple as any
+export const toEq: Schemable1<'io-ts/toEq'> = {
+  URI: 'io-ts/toEq',
+  string: E.string,
+  number: E.number,
+  nullable: E.nullable,
+  tuple: E.tuple
 }
 
 // -------------------------------------------------------------------------------------
 // example
 // -------------------------------------------------------------------------------------
 
-const schema = make((S) => S.fromTuple(S.nullable(S.string)))
+import * as assert from 'assert'
+import { pipe } from 'fp-ts/lib/pipeable'
+
+const schema = make((S) => S.tuple(S.nullable(S.string)))
 
 export const decoder = compile(toDecoder)(schema)
-console.log(decoder.decode([null]))
-console.log(decoder.decode(['a']))
-console.log(decoder.decode([1]))
+assert.deepStrictEqual(decoder.decode([null]), D.success([null]))
+assert.deepStrictEqual(decoder.decode(['a']), D.success(['a']))
+assert.deepStrictEqual(
+  pipe(decoder.decode([1]), D.draw, D.print),
+  `Errors:
+1 error(s) found while decoding (tuple)
+└─ 1 error(s) found while decoding required component 0
+   └─ 1 error(s) found while decoding a nullable
+      └─ cannot decode 1, expected a string`
+)
 
 export const guard = compile(toGuard)(schema)
-console.log(guard.is([null]))
-console.log(guard.is(['a']))
-console.log(guard.is([1]))
+assert.deepStrictEqual(guard.is([null]), true)
+assert.deepStrictEqual(guard.is(['a']), true)
+assert.deepStrictEqual(guard.is([1]), false)
 
-console.log(JSON.stringify(decoder, null, 2))
+export const eq = compile(toEq)(schema)
+assert.deepStrictEqual(eq.equals([null], [null]), true)
+assert.deepStrictEqual(eq.equals(['a'], ['a']), true)
+assert.deepStrictEqual(eq.equals(['a'], ['b']), false)
